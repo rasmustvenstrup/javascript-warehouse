fcc86d01c5786eda617faacc9960467c8156b65c
Rasmus Hagedorn Tvenstrup <tvenstrup@gmail.com>
2018-11-18 10:29:08 +0100

    Added .css and .js files.

---
+++ products.js
@@ -0,0 +1,50 @@
+
+loadProducts();
+
+function loadProducts() {
+
+    var productsTable = document.getElementById("productsTable");
+
+    if (productsTable != null)
+    {
+        var rows = "<tr><th>Navn</th><th>Beskrivelse</th><th>Pris</th></tr>";
+
+        if (localStorage.products)
+        {
+            var products = JSON.parse(localStorage.products);
+            products.forEach(element => {
+                rows += "<tr><td>" + element.name + "</td><td>" + element.description + "</td><td>" + element.price + "</td></tr>";
+            });   
+        }
+        
+        productsTable.innerHTML = rows;
+    }   
+}
+
+function saveProduct() {
+                
+    var products = [];
+    if (localStorage.products)
+    {
+        products = JSON.parse(localStorage.products);
+    }
+
+
+    var name = document.getElementById("name").value;
+    var description = document.getElementById("description").value;
+    var price = document.getElementById("price").value;
+    
+    var product = {
+        name: name,
+        description: description,
+        price: price
+    };
+
+    products.push(product);
+    localStorage.products = JSON.stringify(products);
+
+    document.getElementById("name").value = "";
+    document.getElementById("description").value  = "";
+    document.getElementById("price").value  = "";
+    alert("Produkt tilføjet")
+}
